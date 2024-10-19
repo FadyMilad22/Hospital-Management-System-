33d0367542f81cbea771f922dc1fd403ee6194db
lobnaSaleh <lobna.saleh2003@gmail.com>
2024-10-20 00:56:10 +0300

    somechanges

--- HospitalManagementSystem2/obj/Debug/net8.0/HospitalManagementSystem2.AssemblyInfo.cs
+++ HospitalManagementSystem2/obj/Debug/net8.0/HospitalManagementSystem2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4364ef2da7f1e83f7589f65029981a27657d22c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d2bc9707946e15900912714b7b5f414fc74b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
