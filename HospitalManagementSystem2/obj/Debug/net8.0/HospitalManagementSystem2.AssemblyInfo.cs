f25739ae42e3f965d07c0c294616dbd65c4c2f96
lobnaSaleh <lobna.saleh2003@gmail.com>
2024-10-20 20:17:24 +0300

    changes3

--- HospitalManagementSystem2/obj/Debug/net8.0/HospitalManagementSystem2.AssemblyInfo.cs
+++ HospitalManagementSystem2/obj/Debug/net8.0/HospitalManagementSystem2.AssemblyInfo.cs
@@ -14,7 +14,13 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6b7ab9a6347d6a4fded92de28d35d4405f8e17")]
+||||||| Stash base
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d2bc9707946e15900912714b7b5f414fc74b80")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d0367542f81cbea771f922dc1fd403ee6194db")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
